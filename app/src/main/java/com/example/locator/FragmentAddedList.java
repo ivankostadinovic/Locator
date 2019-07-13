63746b5f7a1a84613a2ec08e5dac1d5514ae766a
BassuoPC <ivankostadinovic1994@outlook.com>
2019-07-13 18:43:39 +0200

    commit

---
+++ app/src/main/java/com/example/locator/FragmentAddedList.java
@@ -0,0 +1,4 @@
+package com.example.locator;
+
+public class FragmentAddedList {
+}
---
+++ app/src/main/java/com/example/locator/PopUpFeedQuest.java
@@ -0,0 +1,14 @@
+package com.example.locator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopUpFeedQuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_feed_quest);
+    }
+}
